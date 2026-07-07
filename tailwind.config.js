6c827cfbf8dd9cdf2d5506d545c847876875fae5
cobrarfacil <tiagoribeirocabral@hotmail.com>
2026-07-07 20:52:48 -0300

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  corePlugins: {
+    preflight: false,
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
